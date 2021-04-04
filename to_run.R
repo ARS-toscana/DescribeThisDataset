7d60fcc516ed30e38eadb4b4ce707456e3247cd1
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2021-04-04 23:19:08 +0200

    rmarkdown developing

--- to_run.R
+++ to_run.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-
+library(data.table)
 #set the directory where the file is saved as the working directory
 if (!require("rstudioapi")) install.packages("rstudioapi")
 thisdir<-setwd(dirname(rstudioapi::getSourceEditorContext()$path))
@@ -15,27 +15,13 @@ Dataset$am=ifelse(Dataset$am==1, "one", "zero")
 Dataset$vs=ifelse(Dataset$vs==1, TRUE, FALSE)
 
 # Call 
-
-DescribeThisDataset(Dataset=Dataset,
-                    Individual=T,
-                    HeadOfDataset=TRUE,
-                    StructureOfDataset=TRUE,
-                    PathOutputFolder=paste0(thisdir,"/g_describeHTML/"),
-                    NameOutputFile="all"
+library(rmarkdown)
+render("DescribeThisDataset.Rmd", params=list(Dataset=Dataset,
+                                            Individual=T,
+                                            HeadOfDataset=TRUE,
+                                            StructureOfDataset=TRUE,
+                                            PathOutputFolder=paste0(thisdir,"/g_describeHTML/"),
+                                            NameOutputFile="Description of the Dataset")
 )
 
-DescribeThisDataset(Dataset=Dataset,
-                    Individual=T,
-                    Cols=list("cyl", "hp"),
-                    HeadOfDataset=TRUE,
-                    StructureOfDataset=TRUE,
-                    PathOutputFolder=paste0(thisdir,"/g_describeHTML/"),
-                    NameOutputFile="selected_vars"
-)
 
-DescribeThisDataset(Dataset=Dataset,
-                    Individual=T,
-                    Cols=list("cyl", "hp"),
-                    ColsFormat=list("categorical", "continuous"),
-
-)
